de9e1fc296bf18b741e3889b2519b25b860ba292
Torbary <torbarey020@gmail.com>
2023-10-03 11:00:26 +0100

    modified; add 3 new endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 import { Express } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
-
+import AuthController from '../controllers/AuthController'
 
 const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('stats', AppController.getStats);
 
+  api.get('/connect', AuthController.getConnect);
+  api.get('/disconnect', UsersController.getMe);
   api.post('/users', UsersController.postNew);
 };
 
