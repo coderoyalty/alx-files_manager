3e54d9dcb1cf1e2cc7a37fb7373abd35c0953dea
Akanni Emmanuel <akannie33@gmail.com>
2023-10-03 12:34:19 +0100

    fix `/stats` route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 import { Express } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController'
+import AuthController from '../controllers/AuthController';
 
 const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
-  api.get('stats', AppController.getStats);
+  api.get('/stats', AppController.getStats);
 
   api.get('/connect', AuthController.getConnect);
   api.get('/disconnect', UsersController.getMe);
